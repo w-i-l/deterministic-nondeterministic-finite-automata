e24db208c8b5f460f425aa82af25d1a40651fcea
Ocnaru Mihai <masterpaper01@gmail.com>
2023-03-14 09:36:46 +0200

    updated class to inheritance
    corrected a minor bug that would accept bad words on a NFA
    also partially corected the output for a NFA

--- main.py
+++ main.py
@@ -29,10 +29,8 @@ class Node:
         except Exception:
             return [-1]
 
-    
 
-class DFA:
-    
+class FA:
     def __init__(self):
         self.current_state = 0
         self.nodes = []
@@ -52,8 +50,12 @@ class DFA:
     def empty(self):
         self.nodes = []
 
-    # check if a word is accepted
-    # prints on the screen the result
+
+class DFA(FA):
+    
+    def __init__(self):
+        super().__init__()
+
     def validate_word(self, word):
 
         word = list(word)
@@ -84,35 +86,16 @@ class DFA:
             print("neacceptat")
         
     
-
-class NFA:
+class NFA(FA):
     
     def __init__(self):
-        self.current_state = 0
-        self.nodes = []
-        self.result = [0]
+        super().__init__()
         self.accepted = False
 
-    # reads the DFA from filename
-    def read_from(self, filename):
-        with open(filename, 'r') as f:
-            for line in f.readlines():
-                data = line.strip().split()
-                paths = [(data[i+1], int(data[i])) for i in range(2,len(data),2)]
-                final = True if data[1] == 'f' else False
-
-                self.nodes.append(Node(data[0], final, paths))
-
-    # deletes the read nodes
-    def empty(self):
-        self.nodes = []
 
     # check if a word is accepted
     # prints on the screen the result
     def validate_word(self, word, current_state=0):
-
-        # if self.accepted:
-        #     return
 
         word = list(word)
         self.current_state = current_state
@@ -120,7 +103,9 @@ class NFA:
         while len(word) and self.current_state != -1:
 
             # get the current letter to be processed
+            # copy = word.copy()
             current_letter = word.pop(0)
+
 
             # get the next node
             # for i
@@ -128,19 +113,20 @@ class NFA:
             # print(self.current_state, current_letter, word)
 
             states = self.nodes[self.current_state].next(current_letter)
-
+            # print(states, current_letter)
             if len(states) > 1 and self.accepted == False:
                 for state in states:
-                    # print(state, word)
+                    # print(state, current_letter, word)
                     self.validate_word("".join(word), state)
                     if self.accepted:
                         break
+                    # word = copy
             else:
                 self.current_state = states[0]
 
             # check to see if we have an invalid state
             if self.current_state == -1 and self.accepted:
-                print('neacceptat')
+                # print('neacceptat')
                 return
             
             # save the path
@@ -148,18 +134,21 @@ class NFA:
 
         # check if we proceed all letters
         # and we are in a final state
-        if len(word) == 0 and self.nodes[self.current_state].final and self.accepted != True:
+        if len(word) == 0 and self.current_state != -1 and self.nodes[self.current_state].final and self.accepted != True:
             print('acceptat')
             print("Drum: ", *self.result, end=' ')
             self.accepted = True
             print()
-        elif self.accepted == False:
-            print("neacceptat")
+            return
+        # elif self.accepted == False:
+        #     print("neacceptat")
+        #     return
+        # print("neacceptat")
         
 
 
 actual = NFA()
-actual.read_from("nfa2.txt")
-actual.validate_word("")
+actual.read_from("nfa.txt")
+actual.validate_word("addaab")
 # for node in actual.nodes:
 #     node.display()
